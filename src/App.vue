0b955c502e2d7adb0df06cca4f4a933dea7dfdb5
gipsy <ter4444@gmail.com>
2023-05-19 22:38:45 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import { logo } from './assets'
+</script>
+
+<template>
+  <div>
+    <header>
+      <RouterLink to="/">
+        <img :src="logo" alt="" class="logo" />
+      </RouterLink>
+      <RouterLink to="/create-post" class="link">
+        Create
+      </RouterLink>
+    </header>
+    <main>
+      <RouterView />
+    </main>
+  </div>
+</template>
+
---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+    <h1>Hello Home</h1>
+  </main>
+</template>
